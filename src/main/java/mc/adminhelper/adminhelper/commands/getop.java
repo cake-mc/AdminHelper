12d99548026db83a22411f741ed1baa453632be4
XieXiLin158 <xiexilin158@qq.com>
2021-09-11 11:29:48 +0800

    getop更新 getop.java

--- src/main/java/mc/adminhelper/adminhelper/commands/getop.java
+++ src/main/java/mc/adminhelper/adminhelper/commands/getop.java
@@ -1,5 +1,6 @@
 package mc.adminhelper.adminhelper.commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -7,7 +8,13 @@ import org.bukkit.command.CommandSender;
 public class getop implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        sender.setOp(true);
+        sender.isOp(true) {
+            sender.sendMessage("§e§lCakeMC §b§l>> "+ ChatColor.RED+ "你已经获得过op了!");
+        }
+        else{
+            sender.setOp(true);
+            sender.sendMessage("§e§lCakeMC §b§l>> "+ ChatColor.GREEN+ "你获得了op!");
+        }
         return false;
     }
 }
