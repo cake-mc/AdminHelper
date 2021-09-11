fb9ced8c93ea22616078765ca3ad270b269834ea
XieXiLin158 <xiexilin158@qq.com>
2021-09-11 10:33:49 +0800

    Update

--- src/main/java/mc/adminhelper/adminhelper/commands/cakemc.java
+++ src/main/java/mc/adminhelper/adminhelper/commands/cakemc.java
@@ -8,7 +8,7 @@ import org.bukkit.command.CommandSender;
 public class cakemc implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        sender.sendMessage("§e§lCakeMC §b§l>> "+ ChatColor.YELLOW+sender.getName()+ChatColor.GREEN+" 这是测试");
+        sender.sendMessage("§e§lCakeMC §b§l>> "+ ChatColor.YELLOW+sender.getName()+ChatColor.GREEN+" 欢迎来到"+" §e§lCakeMC");
         return false;
     }
 }
